6a90de93160aca140ebfc8df6e5b52b6e089050c
shenglihua <761855930@qq.com>
2021-11-27 09:19:37 +0800

    拉取

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://192.168.1.61:8888/api/private/v1/login', // baseurl 基础的路径地址
+                target: 'http://192.168.1.61:8888/api/private/v1', // baseurl 基础的路径地址
                 changeOrigin: true,// 是否跨域
                 pathRewrite: {
                     '^/api':'' // 重写路径
